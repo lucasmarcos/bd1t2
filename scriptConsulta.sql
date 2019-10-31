2656a28e902b9d20904895d009c76841479bff8d
Lucas Marcos <lucasmarcos@live.com>
2019-10-31 19:32:50 -0300

    estendendo as buscas

--- scriptConsulta.sql
+++ scriptConsulta.sql
@@ -1,3 +1,4 @@
+-- todos os contatos com o nome do consumidor e do atendete
 SELECT consumidor.nome   AS "Consumidor",
        atendente.nome    AS "Atendente",
        contato.descricao AS "Descrição Geral do Contato",
@@ -8,3 +9,15 @@ WHERE contato.consumidor = consumidor.cpf
 ORDER BY contato.inicio,
          consumidor.nome,
          atendente.nome;
+
+-- todos os atendimentos 'Resolvidos' de 'Lucas Marcos'
+SELECT consumidor.nome   AS "Consumidor",
+       contato.descricao AS "Descrição Geral do Contato",
+       contato.status    AS "Status do Contato"
+FROM consumidor, contato
+WHERE contato.consumidor = consumidor.cpf
+  AND consumidor.nome    = 'Lucas Marcos'
+  AND contato.status     = 'Resolvido'
+ORDER BY contato.status,
+         contato.inicio,
+         consumidor.nome;
