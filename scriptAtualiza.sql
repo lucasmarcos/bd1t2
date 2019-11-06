f23c7273240b5f4cfbc7e38084e2bc6f04626d38
aluno <aluno>
2019-11-06 01:19:23 -0200

    luz

--- scriptAtualiza.sql
+++ scriptAtualiza.sql
@@ -1,8 +1,17 @@
-UPDATE consumidor SET tatata WHERE; -- filtro
-UPDATE consumidor SET tatata WHERE; -- unico
+--
+UPDATE consumidor SET nome = '', endereco = '' WHERE cpf = '62691733025';
 
-UPDATE atendente SET tatata WHERE; -- filtro
+--
+UPDATE contato SET tatata WHERE; -- unico
+
+--
 UPDATE atendente SET tatata WHERE; -- unico
 
+--
+UPDATE atendente SET tatata WHERE; -- filtro
+
+--
 UPDATE contato SET tatata WHERE; -- filtro
-UPDATE contato SET tatata WHERE; -- unico
+
+--
+UPDATE consumidor SET tatata WHERE; -- filtro
--- scriptRemove.sql
+++ scriptRemove.sql
@@ -1,11 +1,12 @@
-DELETE FROM consumidor WHERE <>;
-DELETE FROM consumidor WHERE cpf = '';
-DELETE FROM consumidor WHERE rg = '';
+DELETE FROM consumidor WHERE nascimento > current_date;
+DELETE FROM consumidor WHERE cpf = '62691733025';
 
 DELETE FROM atendente WHERE >;
-DELETE FROM atendente WHERE cpf = '';
+DELETE FROM atendente WHERE 
 DELETE FROM atendente WHERE rg = '';
 
 DELETE FROM contato WHERE <;
 DELETE FROM contato WHERE cpf = '';
 DELETE FROM contato WHERE rg = '';
+
+DELETE FROM;
