79f9af1e67c95971b892dbdcd17d7d51a382c63f
Lucas Marcos <lucasmarcos@live.com>
2019-11-07 16:35:41 -0300

    d e t a l h e s

--- scriptAtualiza.sql
+++ scriptAtualiza.sql
@@ -4,20 +4,20 @@
 -- Anderson Pereira Brzezinski
 -- Leonardo Dall Asta Kruger
 
---
+-- atualiza o nome e endereco de um unico consumidor com um cpf especifico
 UPDATE consumidor SET nome = 'Jean Smart', endereco = 'Pato Branco' WHERE cpf = '62691733025';
 
---
-UPDATE contato SET detalhes = 'Agora com mais informações, muito mais detalhes!' WHERE contato.protocolo = 1; -- unico
+-- muda os detalhes de um contato especifico
+UPDATE contato SET detalhes = 'Agora com mais informações, muito mais detalhes!' WHERE contato.protocolo = 1;
 
---
-UPDATE atendente SET setor = 'Reparos' WHERE cracha = 1; -- unico
+-- muda o setor de um unico atendente pelo num. de seu cracha
+UPDATE atendente SET setor = 'Reparos' WHERE cracha = 1;
 
---
-UPDATE medida_interna_tomada SET status = 'Urgente' WHERE status = 'Atrasada'; -- filtro
+-- marca como 'Urgente' todas as medidas internas 'Atrasadas'
+UPDATE medida_interna_tomada SET status = 'Urgente' WHERE status = 'Atrasada';
 
---
-UPDATE resposta SET prazo = (prazo + interval '1 week') WHERE prazo > (current_date - interval '2 days'); -- filtro
+-- aumenta em uma semana o prazo de todas as repostas que tem apenas mais 2 dias para serem concluidas
+UPDATE resposta SET prazo = (prazo + interval '1 week') WHERE prazo < (current_date + interval '2 days');
 
---
-UPDATE setor SET responsavel = 1 WHERE responsavel = NULL; -- filtro
+-- define o empregado de cracha 1 como resposavel de todos os setores sem resposaveis
+UPDATE setor SET responsavel = 1 WHERE responsavel = NULL;
